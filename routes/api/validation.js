8f25894031dbb785397183e4af3a075d818b3f44
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-07 10:32:51 +0300

    make validation with joi

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,14 +1,9 @@
 const Joi = require("joi");
 
 const schemaContacts = Joi.object({
-  name: Joi.string().alphanum().min(1).max(20).required(),
+  name: Joi.string().min(1).max(20).required(),
   phone: Joi.number().integer().positive().min(10).max(11).required(),
   email: Joi.string().email().optional(),
 });
 
-try {
-  const value = await schema.validateAsync({
-    username: "abc",
-    birth_year: 1994,
-  });
-} catch (err) {}
+const validate = (schema, obj, res, next) => {};
